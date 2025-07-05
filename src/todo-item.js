ce010b10ac69d731acbfcd8ce05b568a8fba3584
CollinDG <154558368+CollinDG@users.noreply.github.com>
2025-07-05 12:23:09 -0700

    Create TodoItem class

---
+++ src/todo-item.js
@@ -0,0 +1,27 @@
+export class TodoItem {
+    constructor(title, description, dueDate, priority, completed) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.completed = completed;
+    }
+
+    get dueDate() {
+        return this._dueDate;
+    }
+
+    set dueDate(value) {
+        this._dueDate = value;
+    }
+
+    get priority() {
+        return this._priority;
+    }
+
+    set priority(value) {
+        const levels = ["low", "medium", "high"];
+        if (!levels.includes(value)) throw new Error("Invalid priority level");
+        this._priority = value;
+    }
+}
