bd2e484d228f496a11b37c165164247ad71a4cfb
CollinDG <154558368+CollinDG@users.noreply.github.com>
2025-07-11 13:28:15 -0700

    Load todo list from local storage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import { addEventListeners, renderPage } from "./dom";
+import { TodoItem } from "./todo-item";
+import { setTodos } from "./todo-list";
 import "./styles.css"
 
-renderPage("inbox");
-addEventListeners();
+function loadSite() {
+    const raw = JSON.parse(localStorage.getItem("todos")) || [];
+    setTodos(raw.map(todo => new TodoItem(
+        todo.name,
+        todo.description,
+        todo.priority,
+        new Date(todo._dueDate)
+    )));
+    renderPage("inbox");
+    addEventListeners();
+}
+
+loadSite();
