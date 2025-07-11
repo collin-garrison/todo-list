e1f1f75f840d3cc9d5b0f8470e19b9c5e916ddff
CollinDG <154558368+CollinDG@users.noreply.github.com>
2025-07-11 13:03:24 -0700

    Store todo list in localStorage

--- src/todo-list.js
+++ src/todo-list.js
@@ -4,8 +4,9 @@ const todos = [];
 
 export function addTodo(name, description, priority, dueDate) {
     description = description?.trim() || "";
-    const newTodo = new TodoItem(name, description, priority, dueDate, false);
+    const newTodo = new TodoItem(name, description, priority, dueDate);
     todos.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
 };
 
 export const getTodos = () => todos;
